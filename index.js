8c17f571dbef0667821a99c85697314c16f1acca
Caio Santos <caio.santos@msn.com>
2021-12-19 23:00:27 +0900

    add the background of the game black

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello")
+const canvas = document.getElementById("gameArea");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
